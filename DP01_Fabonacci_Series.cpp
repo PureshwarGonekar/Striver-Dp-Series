a1192e53cd98a0a0774b79b6300d7115f7ab001a
Pureshwar <pureshwargonekar7@gmail.com>
2023-06-17 23:51:06 +0530

    Dp 01 solved

---
+++ DP01_Fabonacci_Series.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+// Normal Approch Time Complexity - O(2^n) Space Complexity - O(n)[function stack]
+int fibo1(int n){
+    if(n<=1){
+        return n;
+    }
+    int a = fibo1(n-1);
+    int b = fibo1(n-2);
+    return a+b;
+}
+
+//Memorization Time Complexity - O(n)  Space Complexity - O(n)[function stack]+ O(n)[array of size n+1]
+int fibo2(int n, int *ans){
+    if(n<=1){
+        return n;
+    }
+    if(ans[n]!= -1){
+        return ans[n];
+    }
+    int a = fibo1(n-1);
+    int b = fibo1(n-2);
+    ans[n]= a+b;
+    return ans[n];
+}
+int fibo2(int n){
+    int *ans = new int[n+1];
+    for(int i=0;i<=n;i++){
+        ans[i]=-1;
+    }
+    return fibo2(n,ans);
+}
+
+// Tabular Method (DP) Time Complexity - O(n)  Space Complexity - O(n)
+int fibo3(int n){
+    int *ans = new int[n+1];
+    ans[0]=0;
+    ans[1]=1;
+    for(int i=2;i<=n;i++){
+        ans[i]= ans[i-1]+ans[i-2];
+    }
+    return ans[n];
+}
+int main(){
+    cout<<fibo1(10)<<endl;
+    cout<<fibo2(20)<<endl;
+    cout<<fibo3(30)<<endl;
+
+    return 0;
+}
