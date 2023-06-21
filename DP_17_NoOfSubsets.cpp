a3b2775dd924dd87badcb36752de59b6c9499b94
Pureshwar <pureshwargonekar7@gmail.com>
2023-06-21 14:10:41 +0530

    now able to run for negative no also

--- DP_17_NoOfSubsets.cpp
+++ DP_17_NoOfSubsets.cpp
@@ -54,9 +54,12 @@ int findWays(vector<int> &num, int tar)
 {
     int n= num.size();
     vector<vector<int>> dp(n,vector<int>(tar+1,0));
-    for(int i=0;i<n;i++) dp[i][0] = 1;
-    if(num[0]<=tar) dp[0][num[0]] = 1;
-
+    //for(int i=0;i<n;i++) dp[i][0] = 1;
+    if(num[0]==0) dp[0][0]= 2; // if num[0] = 0 then if target =0 then there is 2 ways.
+    else dp[0][0] =1;
+     
+    //if(num[0]<=tar) dp[0][num[0]] = 1; // fail if num[0] = 0 , it will overwrite the above if condition
+    if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;
     for(int ind=1;ind<n;ind++){
         for(int sum=0;sum<=tar;sum++){
             int not_take= dp[ind-1][sum];
