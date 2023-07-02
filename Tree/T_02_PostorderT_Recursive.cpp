065c4acb395e5a9968de19627f42869984c6e9cf
Pureshwar <pureshwargonekar7@gmail.com>
2023-07-02 13:59:46 +0530

    postorder

---
+++ Tree/T_02_PostorderT_Recursive.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * template <typename T>
+ * struct TreeNode {
+ *     T val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(T x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(T x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+
+*/
+
+void preorder(TreeNode<int> *root){
+    if(root == NULL)
+        return;
+    preorder(root->left);
+    preorder(root->right);
+    cout<<root->data<<endl;
+}
