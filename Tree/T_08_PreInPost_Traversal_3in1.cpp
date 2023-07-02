00890dce860894061ec0f9b3ae2d94fa710cec1e
Pureshwar <pureshwargonekar7@gmail.com>
2023-07-02 18:08:12 +0530

    3 in 1 using stack

---
+++ Tree/T_08_PreInPost_Traversal_3in1.cpp
@@ -0,0 +1,34 @@
+// Problem - https://www.codingninjas.com/studio/problems/level-order-traversal_796002
+
+vector<int> preInPostTraversal(BinaryTreeNode *root){
+    stack<pair <BinaryTreeNode*, int>> st;
+    st.push({root,1});
+    vector<int> pre, in ,post;
+    if(root== NULL) return;
+
+    while(!st.empty()){
+        auto it = st.top();
+        st.pop();
+
+        if(it.second==1){
+            pre.push_back(it.first->val);
+            it.second++;
+            st.push(it);
+            if(it.first->left !=NULL){
+                st.push({it.first->left,1});
+            }
+        }
+        else if(it.second==2){
+            in.push_back(it.first->val);
+            it.second++;
+            st.push(it);
+            if(it.first->right !=NULL){
+                st.push({it.first->right,1});
+            }
+        }
+        else{
+            post.push_back(it.first->val);
+        }
+    }
+    return in;
+}
