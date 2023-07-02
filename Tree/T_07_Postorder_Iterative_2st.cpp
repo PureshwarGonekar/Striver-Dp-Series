8ea17962aae5c91ac4698f26c8dceb581a9ac0fc
Pureshwar <pureshwargonekar7@gmail.com>
2023-07-02 15:31:15 +0530

    solved using 2 stack

---
+++ Tree/T_07_Postorder_Iterative_2st.cpp
@@ -0,0 +1,26 @@
+vector<int> postorderT(TreeNode* root) {
+    vector<int> postorder;
+    if(root == NULL) return postorder;
+    stack<TreeNode*> st1, st2;
+    st1.push(root);
+    while(!st1.empty() ){
+
+        root = st1.top();
+        st1.pop();
+        st2.push(root);
+
+        if(root->left= NULL) {
+            st1.push(root->Left);
+        }
+        if(root->right != NULL) {
+            st1.push(roct->right);
+        }
+
+    }
+    while(!st2.empty()){
+        postorder.push_back(st2.top()->val);
+        st2.pop();
+    }
+    return postorder;
+
+}
