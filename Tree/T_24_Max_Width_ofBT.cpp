c2b7b9f87e69094e474b84291094c61b658cbd26
Pureshwar <pureshwargonekar7@gmail.com>
2023-07-05 16:11:17 +0530

    CN solution added

--- Tree/T_24_Max_Width_ofBT.cpp
+++ Tree/T_24_Max_Width_ofBT.cpp
@@ -1,4 +1,4 @@
-
+// Leetcode - https://leetcode.com/problems/maximum-width-of-binary-tree/submissions/986852448/
 
 int widthOfBinaryTree(TreeNode * root) {
     if (!root) return 0;
@@ -21,6 +21,31 @@ int widthOfBinaryTree(TreeNode * root) {
                 q.push({temp -> right,(long long)cur_id * 2 + 2});
         }
         ans = max(ans, rightMost - leftMost + 1);
+    }
+    return ans;
+}
+
+
+// Coding Ninjas - https://www.codingninjas.com/studio/problems/maximum-width-in-binary-tree_763671
+
+//In this we only max no of non-null node comparing each level using Level order Traversal
+int getMaxWidth(TreeNode<int> *root)
+{
+    if (!root) return 0;
+    int ans = 0;
+    queue <TreeNode<int> * > q;
+    q.push(root);
+    while (!q.empty()) {
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            TreeNode<int> * temp = q.front();
+            q.pop();
+            if (temp -> left)
+                q.push(temp -> left); // cur_id*2 +1 then it convert to INT_Max 
+            if (temp -> right)
+                q.push(temp -> right);
+        }
+        ans = max(ans, size);
     }
     return ans;
 }
