87629c73f992835c451aa7ce821664eb06c00b07
Pureshwar <pureshwargonekar7@gmail.com>
2023-07-05 15:55:39 +0530

    fixed overflow condition

---
+++ Tree/T_24_Max_Width_ofBT.cpp
@@ -0,0 +1,26 @@
+
+
+int widthOfBinaryTree(TreeNode * root) {
+    if (!root) return 0;
+    int ans = 0;
+    queue < pair < TreeNode * , int >> q;
+    q.push({root,0});
+    while (!q.empty()) {
+        int size = q.size();
+        int curMin = q.front().second;
+        int leftMost, rightMost;
+        for (int i = 0; i < size; i++) {
+            int cur_id = q.front().second - curMin; // subtracted to prevent integer overflow
+            TreeNode * temp = q.front().first;
+            q.pop();
+            if (i == 0) leftMost = cur_id;
+            if (i == size - 1) rightMost = cur_id;
+            if (temp -> left)
+                q.push({temp -> left,(long long)cur_id * 2 + 1}); // cur_id*2 +1 then it convert to INT_Max 
+            if (temp -> right)
+                q.push({temp -> right,(long long)cur_id * 2 + 2});
+        }
+        ans = max(ans, rightMost - leftMost + 1);
+    }
+    return ans;
+}
