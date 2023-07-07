d0778398ecb47521bbc084d41b27fbb2b5d0bfad
Pureshwar <pureshwargonekar7@gmail.com>
2023-07-07 17:20:42 +0530

    Construct BT Inorder and Preorder

---
+++ Tree/T_28_Tree_from_IN_Preorder.cpp
@@ -0,0 +1,30 @@
+// Problem -  https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/submissions/988540485/
+//CN https://www.codingninjas.com/studio/problems/construct-a-binary-tree-from-preorder-and-inorder-traversal_920539
+
+TreeNode* constructTree(vector < int > & preorder, int preStart, int preEnd, vector 
+ < int > & inorder, int inStart, int inEnd, map < int, int > & mp) {
+  if (preStart > preEnd || inStart > inEnd) return NULL;
+
+  TreeNode* root = newNode(preorder[preStart]);
+  int elem = mp[root -> data];
+  int nElem = elem - inStart;
+
+  root -> left = constructTree(preorder, preStart + 1, preStart + nElem, inorder,
+  inStart, elem - 1, mp);
+  root -> right = constructTree(preorder, preStart + nElem + 1, preEnd, inorder, 
+  elem + 1, inEnd, mp);
+
+  return root;
+}
+
+TreeNode* buildTree(vector < int > & preorder, vector < int > & inorder) {
+  int preStart = 0, preEnd = preorder.size() - 1;
+  int inStart = 0, inEnd = inorder.size() - 1;
+
+  map < int, int > mp;
+  for (int i = inStart; i <= inEnd; i++) {
+    mp[inorder[i]] = i;
+  }
+
+  return constructTree(preorder, preStart, preEnd, inorder, inStart, inEnd, mp);
+}
