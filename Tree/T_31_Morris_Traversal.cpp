e9faeeee45d76095ef0e054c7f1ff3a1df4e837a
Pureshwar <pureshwargonekar7@gmail.com>
2023-07-09 23:59:39 +0530

    post order traversal added

--- Tree/T_31_Morris_Traversal.cpp
+++ Tree/T_31_Morris_Traversal.cpp
@@ -26,3 +26,32 @@ vector < int > inorderTraversal(node * root) {
   }
   return inorder;
 }
+
+
+// Post order Traversal T.C = O(N) S.C. O(1)
+vector < int > inorderTraversal(node * root) {
+  vector < int > inorder;
+
+  node * cur = root;
+  while (cur != NULL) {
+    if (cur -> left == NULL) {
+      inorder.push_back(cur -> data);
+      cur = cur -> right;
+    } else {
+      node * prev = cur -> left;
+      while (prev -> right != NULL && prev -> right != cur) {
+        prev = prev -> right;
+      }
+
+      if (prev -> right == NULL) {
+        inorder.push_back(cur -> data);
+        prev -> right = cur;
+        cur = cur -> left;
+      } else {
+        prev -> right = NULL;
+        cur = cur -> right;
+      }
+    }
+  }
+  return inorder;
+}
