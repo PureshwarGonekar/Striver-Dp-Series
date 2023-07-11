ad4dce8f37bf997088db635d8abc4ee4e7930496
Pureshwar <pureshwargonekar7@gmail.com>
2023-07-11 13:35:32 +0530

    inorder traversal

---
+++ Tree/T_45_Recover_BST.cpp
@@ -0,0 +1,26 @@
+// Problem - https://leetcode.com/problems/recover-binary-search-tree/
+
+class Solution {
+	TreeNode* firstMistake, *secondMistake, *pre;
+public:
+	void recoverTree(TreeNode* root) {
+		pre = new TreeNode(INT_MIN);
+		inorder(root);
+		swap(firstMistake->val, secondMistake->val);
+	}
+
+	void inorder(TreeNode* root) {
+		if(root == nullptr) 
+			return;
+
+		inorder(root->left);
+
+		if(firstMistake == nullptr && root->val < pre->val)
+			firstMistake = pre;
+		if(firstMistake != nullptr && root->val < pre->val)
+			secondMistake = root;
+		pre = root;
+
+		inorder(root->right);
+	}
+};
