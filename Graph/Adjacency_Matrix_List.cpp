fd5fe3452a4d2afcaec84304c89fa20af57848e9
Pureshwar <pureshwargonekar7@gmail.com>
2023-07-12 09:43:07 +0530

    storing weighted graph

--- Graph/Adjacency_Matrix_List.cpp
+++ Graph/Adjacency_Matrix_List.cpp
@@ -12,8 +12,8 @@ int main()
     for(int i = 0; i < m; i++)
     {
         int u, v;
-        cin >> u >> v;
-        adj[u][v] = 1;
+        cin >> u >> v; // cin>>wt;
+        adj[u][v] = 1; // assign wt in case of weighted graph
         adj[v][u] = 1  // this statement will be removed in case of directed graph
     }
     return 0;
@@ -34,6 +34,25 @@ int main()
         cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u); // for directed this statement will be removed T.C= O(E)
+    }
+    return 0;
+}
+
+
+//Storing Weighted Graph in Adjacency list
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    // adjacency list for undirected graph
+    // time complexity: O(2E)
+    vector< pair<int,int> > adj[n+1]; //Space Complexity : O(N)
+    for(int i = 0; i < m; i++)
+    {
+        int u, v,wt;
+        cin >> u >> v>>wt;
+        adj[u].push_back({v,wt});
+        adj[v].push_back({u,wt}); // for directed this statement will be removed T.C= O(E)
     }
     return 0;
 }
