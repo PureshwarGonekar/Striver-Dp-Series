8d752b68ff036fd69279ac68a601cae9a4a7006f
Pureshwar <pureshwargonekar7@gmail.com>
2023-07-29 17:10:28 +0530

    solved using topoSort by reversing the Graph

---
+++ Graph/G_20_EventualSafeState_using_topoSort.cpp
@@ -0,0 +1,42 @@
+//https://practice.geeksforgeeks.org/problems/eventual-safe-states/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=eventual-safe-states
+
+//Time Complexity: O(V+E)+O(N*logN)
+//Space Complexity: O(N) + O(N) + O(N) ~ O(3N)
+
+//Intution : Reverse the edges of graph and apply the toposort
+// for ascending order -> sort the topo vector
+class Solution {
+public:
+	vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+		vector<int> adjRev[V];
+		int indegree[V] = {0};
+		for (int i = 0; i < V; i++) {
+			// i -> it
+			// it -> i
+			for (auto it : adj[i]) {
+				adjRev[it].push_back(i);
+				indegree[i]++;
+			}
+		}
+		queue<int> q;
+		vector<int> safeNodes;
+		for (int i = 0; i < V; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+			}
+		}
+
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			safeNodes.push_back(node);
+			for (auto it : adjRev[node]) {
+				indegree[it]--;
+				if (indegree[it] == 0) q.push(it);
+			}
+		}
+
+		sort(safeNodes.begin(), safeNodes.end());
+		return safeNodes;
+	}
+};
