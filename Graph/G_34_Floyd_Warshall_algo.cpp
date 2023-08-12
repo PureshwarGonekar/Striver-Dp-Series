710bc2a45f3b9dc9813ef147284c982bb89b41d9
Pureshwar <pureshwargonekar7@gmail.com>
2023-08-12 17:48:55 +0530

    Floy Warshall Algo with -ve cycle detection

---
+++ Graph/G_34_Floyd_Warshall_algo.cpp
@@ -0,0 +1,45 @@
+//Problem - https://practice.geeksforgeeks.org/problems/implementing-floyd-warshall2042/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=implementing-floyd-warshall
+
+
+//if we find that the cost of reaching any node from itself is less than 0, we can conclude that the graph has a negative cycle.
+//T.C = O(V^3)
+//S.C = O(V^2)
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix){
+	    int n = matrix.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == -1) {
+					matrix[i][j] = 1e9;
+				}
+				if (i == j) matrix[i][j] = 0;
+			}
+		}
+
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					matrix[i][j] = min(matrix[i][j],
+					                   matrix[i][k] + matrix[k][j]);
+				}
+			}
+		}
+
+
+
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == 1e9) {
+					matrix[i][j] = -1;
+				}
+			}
+		}
+	}
+};
+
+
+/*
+if we apply Dijkstra’s algorithm for the same purpose the time complexity reduces to O(V*(E*logV)) 
+*/
